5fbcd87ba1fde5c3d700a254cd66a8e791f141a6
KANNIGA PARAMESHWARI R G <kani71521@gmail.com>
2025-11-08 10:04:25 +0530

    sprint1 project

---
+++ shoppingmallmanagement/src/main/java/com/tnsif/adminservices/AdminServicesApplication.java
@@ -0,0 +1,11 @@
+package com.tnsif.adminservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServicesApplication.class, args);
+    }
+}
