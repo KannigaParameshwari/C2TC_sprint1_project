80073125643d6412a5db113c5fbe8c7c2111861e
KannigaParameshwari <kani71521@gmail.com>
2025-11-08 10:01:10 +0530

    Delete shoppingmallmanagement directory

--- shoppingmallmanagement/src/main/java/com/tnsif/adminservices/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.tnsif.adminservices.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-    // Future configuration beans (like CORS, Security, etc.)
-}
